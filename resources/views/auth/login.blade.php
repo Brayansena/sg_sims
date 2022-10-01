82eb8c692e6d3d06863244ff46dd6589e88ef879
brayan <bepanto6@misena.edu.co>
2022-10-01 09:45:19 -0500

    final

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,7 +12,7 @@
                         @csrf
 
                         <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Nombre de Usuario') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Numero de Telefono') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="text" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
